ff20bb85fa82012d714357bc103feb96bf1c95a6
kunal.kakade <kunal.kakade@rakuten.com>
2020-08-06 22:04:25 +0900

    fix cards alignment

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,28 +7,42 @@ import Card from "./Card/Card";
 import Card2 from "./Card/Card2";
 
 function App() {
-  return (
-    <div>
-      <TopNavbar />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      {/* <h1>Finpeg-Assignment</h1> */}
-      <div className="container">
-        <Card2 />
-        <Card2 />
-        <Card2 />
-        <Card2 />
-        <Card2 />
-        <Card2 />
-        <Card2 />
-        <Card2 />
-      </div>
-      <BottomNavbar />
-    </div>
-  );
+    return (
+        <div>
+            <TopNavbar/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            {/* <h1>Finpeg-Assignment</h1> */}
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                        <Card2/>
+                    </div>
+                </div>
+
+            </div>
+            <BottomNavbar/>
+        </div>
+    );
 }
 
 export default App;
